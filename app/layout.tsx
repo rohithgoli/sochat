bce3fc8831a281dfddcd1b59cdd98a85d33e82c2
Rohith Goli <rohithgoli396@gmail.com>
2024-05-04 13:12:50 +0530

    feat: added assets-icons,images and updated layout with font,metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,30 @@
-import { ClerkProvider } from '@clerk/nextjs'
-import './globals.css'
 import React from 'react'
+import { ClerkProvider } from '@clerk/nextjs'
+import { Inter, Space_Grotesk as SpaceGrotesk } from 'next/font/google'
+import type { Metadata } from 'next'
+import './globals.css'
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500',
+    '600', '700', '800', '900'
+  ],
+  variable: '--font-inter'
+})
+
+const spaceGrotesk = SpaceGrotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-spaceGrotesk'
+})
+
+export const metadata: Metadata = {
+  title: 'Sochat',
+  description: 'A Social platform to muse for asking and answering. Explore topics under the sun. Think, Express in a thoughful way. Get inspired by others state of deep thoughts',
+  icons: {
+    icon: '/assets/images/site-logo.svg'
+  }
+}
 
 export default function RootLayout ({
   children
@@ -8,9 +32,19 @@ export default function RootLayout ({
     children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: 'primary-gradient',
+          footerActionLink: 'primary-text-gradient hover:text-primary-500'
+        }
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}
+        >
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   )
