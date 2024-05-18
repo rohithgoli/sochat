5ede265311c9f041715e19b76aa1387023bc0cc7
Rohith Goli <rohithgoli396@gmail.com>
2024-05-18 18:35:26 +0530

    feat: added theme-switcher with component from shadcn, context hooks, providers

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter, Space_Grotesk as SpaceGrotesk } from 'next/font/google'
 import type { Metadata } from 'next'
 
 import './globals.css'
-import { ThemeProvider } from '../context/ThemeProvider'
+import { ThemeProvider } from '@/context/ThemeProvider'
 
 const inter = Inter({
   subsets: ['latin'],
